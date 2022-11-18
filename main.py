f61db76daa2d01a3284ff0cebb88a42c5175bd81
fierperle <56561566+fierperle@users.noreply.github.com>
2022-11-18 09:53:07 +0100

    added exercice 2 in main

--- main.py
+++ main.py
@@ -1,6 +1,13 @@
 import TP1_2_Calendrier as TP12
+import TP1_3_Impots as TP13
 
 #exercice 1
 print("--- Exercice 2 : Calendrier ---")
 TP12.inputDate()
+print("")
+
+
+#exercice 2
+print("--- Exercice 3 : les impots ---")
+TP12.calcImpot()
 print("")
